18f46ac351ea75f50cb5b0569ae7ad1c35e83857
fcj <fabic@laptop.RandomBinaryThoughts.com>
2016-06-17 10:20:09 +0200

    FABIC: fabic/test/test1.cpp

--- fabic/test/test1.cpp
+++ fabic/test/test1.cpp
@@ -12,12 +12,18 @@
  * FabiC 2016-06-16 https://github.com/fabic/libcsdbg/
  */
 
+#include <dlfcn.h>
+#include <cxxabi.h>
+
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <typeinfo>
 
 // We pass -I.. for compiling so that we may do this
 // (because include/... isn't "namespaced").
-#include "libcsdbg/include/tracer.hpp"
+//#include "libcsdbg/include/tracer.hpp"
 
 namespace fabic {
     namespace plays {
@@ -83,6 +89,59 @@ namespace fabic {
 
             return args;
         }
+
+
+        void terminate_handler() {
+            std::cerr << "Hey! that's terminate!"
+            << '(' << __FILE__
+            << ':' << __LINE__ << ')' << std::endl;
+        }
+
+
+        extern "C" {
+            void __cxa_throw(void *ex, std::type_info *info, void (*dest)(void *)) {
+                // exception_name = demangle(reinterpret_cast<const std::type_info*>(info)->name());
+                // last_size = backtrace(last_frames, sizeof last_frames/sizeof(void*));
+
+                typedef void (*const cxa_throw_func_ptr_type)(void *_ex, std::type_info *_info, void (*_dest)(void *))
+                        __attribute__ ((noreturn));
+
+                // man dlsym
+                auto rethrow = (cxa_throw_func_ptr_type) dlsym(RTLD_NEXT, "__cxa_throw");
+
+                if (rethrow == NULL) {
+                    abort();
+                }
+
+                auto exception_name = [&info]() {
+                    if (info == nullptr)
+                        return std::string("no_exception_type_info");
+
+                    int status = 0xdeadbeef;
+                    char *name = abi::__cxa_demangle(
+                            info->name(),
+                            nullptr, // buffer
+                            nullptr, // buffer length
+                            &status  // demangling exit status
+                    );
+
+                    if (name == nullptr)
+                        return std::string("unknown_exception");
+
+                    assert( status == 0 );
+
+                    auto retv = std::string(name);
+
+                    free( name );
+
+                    return retv;
+                }();
+
+                std::cout << "Thrown ex. " << exception_name << std::endl;
+
+                rethrow(ex, info, dest);
+            }
+        }
     } // plays ns.
 } // fabic ns.
 
@@ -93,7 +152,6 @@ namespace fabic {
  */
 int main_bis(int argc, const char *argv[])
 {
-
     try {
         auto args = fabic::plays::process_program_arguments(argc, argv);
         auto exit_status = fabic::plays::main(args);
@@ -114,14 +172,20 @@ int main_bis(int argc, const char *argv[])
  */
 int main(int argc, const char *argv[])
 {
-    // Init. libcsdbg's tracer thing :
-    using namespace csdbg;
+    std::set_terminate(fabic::plays::terminate_handler);
 
-    tracer *iface = tracer::interface();
-    if ( unlikely(iface == NULL) ) {
-        std::cerr << "FAILED! couldn't initialize libcsdbg's tracer thing." << std::endl;
-        return 126;
-    }
+//    static void (*const rethrow)(void*,void*,void(*)(void*)) __attribute__ ((noreturn))
+//            = (void (*)(void*,void*,void(*)(void*)))dlsym(RTLD_NEXT, "__cxa_throw");
+
+    // Init. libcsdbg's tracer thing :
+    // using namespace csdbg;
+    //
+    // tracer *iface = tracer::interface();
+    // if ( unlikely(iface == NULL) ) {
+    //     std::cerr << "FAILED! couldn't initialize libcsdbg's tracer thing." << std::endl;
+    //     return 126;
+    // }
+
 
     try {
         return main_bis(argc, argv);
